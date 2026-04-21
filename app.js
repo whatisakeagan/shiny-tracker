760f58ce4ff3dcb6264cd423b40e60bd96c3b0d6
Keagan McPherson <whatisakeagan@users.noreply.github.com>
2026-04-21 14:28:16 -0500

    Add hardcoded preliminary data

---
+++ app.js
@@ -0,0 +1,19 @@
+const pokemon = [
+    {id: 1, name: "Bulbasaur"},
+    {id: 4, name: "Charmander"},
+    {id: 7, name: "Squirtle"},
+    {id: 25, name: "Pikachu"},
+    {id: 133, name: "Eevee"},
+    {id: 143, name: "Snorlax"},
+    {id: 150, name: "Mewtwo"},
+    {id: 151, name: "Mew"},
+    {id: 196, name: "Espeon"},
+    {id: 197, name: "Umbreon"},
+    {id: 249, name: "Lugia"}, 
+    {id: 250, name: "Ho-Oh"},
+    {id: 384, name: "Rayquaza"},
+    {id: 448, name: "Lucario"}, 
+    {id: 493, name: "Arceus"}
+];
+
+console.log(pokemon);
